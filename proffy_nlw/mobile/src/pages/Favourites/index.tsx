15b4938978931ea785036cfd3936bc387bffbb56
Darguima <dsgdevbraga@gmail.com>
2020-08-08 02:55:41 +0100

    Proffy - NLW - Mobile (Expo) Started

---
+++ proffy_nlw/mobile/src/pages/Favourites/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import PageHeader from '../../Components/PageHeader';
+
+import styles from './styles';
+
+const Favourites: React.FC = () => {
+  return (
+
+    <View style={styles.container}>
+
+      <PageHeader title="Meus proffys favoritos"/>
+
+    </View>
+
+  );
+}
+
+export default Favourites;
