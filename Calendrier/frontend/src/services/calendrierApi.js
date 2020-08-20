e5086446efd490344d07249c2ede449f9c8a8bb5
Darguima <dsgdevbraga@gmail.com>
2020-08-20 12:03:43 +0100

    Calendrier 2- project creation

--- Calendrier/frontend/src/services/calendrierApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const calendrierApi = axios.create({baseURL: "https://3001-fa2a6196-ab62-4f81-881c-deec5e1119ad.ws-eu01.gitpod.io/api"})
-// RP_server: http://192.168.1.60/api
-// mg_server: http://10.42.0.152/api
-// External acess: http://dsgdevbraga.ddns.net/api
-// Gitpod: https://3001-fa2a6196-ab62-4f81-881c-deec5e1119ad.ws-eu01.gitpod.io/api
-
-export default calendrierApi
--- Calendrier/frontend/src/utils/convertHourToMinutes.js
+++
@@ -1,5 +0,0 @@
-export default function convertHourToMinutes(time){
-	const [hour, minutes] = time.split(":").map(Number)
-
-	return (hour * 60) + minutes
-}
