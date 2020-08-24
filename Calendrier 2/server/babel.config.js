264b18413f9ae3efa3f81637bc315a130942e5dd
Darguima <dsgdevbraga@gmail.com>
2020-08-24 01:30:34 +0100

    Calendrier 2- back-end initial setup

---
+++ Calendrier 2/server/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@name_path': './src/path/path/name_path'
+      }
+    }]
+  ],
+
+  ignore: [
+  ]
+}
